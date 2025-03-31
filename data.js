6d01097e2f61b3927e99c30602a1764d68dbe2a3
Luffy-Montoya <80363512+Luffy-Montoya@users.noreply.github.com>
2025-03-31 12:30:08 -0700

    Update data.js

--- data.js
+++ data.js
@@ -10,14 +10,14 @@ export const menuArray = [
         name: "Hamburger",
         ingredients: ["beef", "cheese", "lettuce"],
         price: 12,
-        image: "/Mobile Ordering App/burger.png",
+        image: "/burger.png",
         id: 1
     },
         {
         name: "Beer",
         ingredients: ["grain, hops, yeast, water"],
         price: 9,
-        image: "https://raw.githubusercontent.com/Luffy-Montoya/Mobile-Ordering-App/refs/heads/main/beer.png",
+        image: "/beer.png",
         id: 2
     }
 ] 
